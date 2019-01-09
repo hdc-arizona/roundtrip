e5e09885730107ea38c0fdd15c6f429449785660
Jesse Bartels <jessebartels@dhcp-10-134-236-7.uawifi.arizona.edu>
2019-01-09 14:14:05 -0700

    Moving over and cleaning up stuff for Roundtrip

---
+++ Examples/temp.js
@@ -0,0 +1,1 @@
+var valueToReadIn = 12;
---
+++ Examples/walkThroughArgListNoScriptTagJustJavascript().js
@@ -0,0 +1,24 @@
+(function(element) {
+    require(['d3'], function(d3) {
+        for (var walk = 0; walk < argList.length; walk++) {
+            //console.log(argList[walk]);
+        }
+        logText(argList)
+        function logText(data) {
+            //console.log(data);
+            var svg = d3.select(element.get(0)).append('svg')
+                .attr('width', 400)
+                .attr('height', 80);
+            svg.selectAll('text')
+                .data(data)
+                .enter()
+                .append('text')
+                .attr("x", function(d) {return 25 * (d);})
+                .attr("y", function(d) {return 15 * (d);})
+                .attr("font-size", "22px")
+                .attr("font-family", "sans-serif")
+                .text( function(d) { return d; })
+            ;
+        }
+    })
+})(element);
